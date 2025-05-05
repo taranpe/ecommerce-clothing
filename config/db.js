850719fce165ee20753f50f848b0f55f774c6bec
Taran Preet <kaurtaran7952gmail.com>
2025-05-05 18:13:14 +0530

    fix: use dynamic port for Railway

--- config/db.js
+++ config/db.js
@@ -1,12 +1,9 @@
-// config/db.js
-const mysql = require('mysql2');
+require('dotenv').config(); // Load env vars
 
-// Create a MySQL connection pool
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',  // Replace with your database password
-    database: 'ecommerce_db'  // Replace with your database name
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
-
-module.exports = pool.promise();
